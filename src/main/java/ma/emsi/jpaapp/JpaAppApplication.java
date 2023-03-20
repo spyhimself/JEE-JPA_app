f57fa35031cd3c88e4d83b534906cc69c56ee369
spyhimself <aymantarar7@gmail.com>
2023-03-20 12:09:49 +0000

    Added data to the in-memory h2 database

--- src/main/java/ma/emsi/jpaapp/JpaAppApplication.java
+++ src/main/java/ma/emsi/jpaapp/JpaAppApplication.java
@@ -1,18 +1,27 @@
 package ma.emsi.jpaapp;
 
+import ma.emsi.jpaapp.entities.Patient;
+import ma.emsi.jpaapp.repository.PatientRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Date;
+
 @SpringBootApplication
 public class JpaAppApplication implements CommandLineRunner {
 
+    @Autowired
+    private PatientRepository patientRepository;
     public static void main(String[] args) {
         SpringApplication.run(JpaAppApplication.class, args);
     }
 
     @Override
     public void run(String... args) throws Exception {
-
+        patientRepository.save(new Patient(null, "Ayman", new Date(), false, 56));
+        patientRepository.save(new Patient(null, "Med", new Date(), true, 82));
+        patientRepository.save(new Patient(null, "Amina", new Date(), false, 24));
     }
 }
